985a8458392c2866ad4c0b5f0ee31cc9295ddb09
Paul Kasko <KaskoPaul.mail@gmail.com>
2014-12-21 19:32:49 +0100

    added Show instance

--- src/Molekuls.hs
+++ src/Molekuls.hs
@@ -2,15 +2,16 @@
 module Molekuls where
 
 import Data.List
+import Data.Ord 
 import qualified Elements as Elements
 
 data Molekul = Molekul {content :: [Elements.Element]} 
 
 instance Show Molekul where
     show (Molekul cnt) = 
-        foldl (\str (atom, num) -> str ++ atom ++ if num > 1 then (show num) else "" ) "" lst
-        where lst = map (\xs@(x:_) -> ((Elements.name x), length xs)) . group . sort $ cnt
-              
+        foldl (\str (atom, num) -> str ++ atom ++ if num > 1 then show num else "") "" lst
+        where sorted = map (\xs@(x:_) -> ((Elements.name x), length xs)) . group . sort $ cnt
+              lst    = sortBy (comparing snd) sorted
     
 
     
