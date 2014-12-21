5ea75ba1f4ed90b99ff4da0ae13a9bf21335a528
Paul Kasko <KaskoPaul.mail@gmail.com>
2014-12-21 20:47:13 +0100

    developed easier concept for input

--- src/Molekuls.hs
+++ src/Molekuls.hs
@@ -5,12 +5,12 @@ import Data.List
 import Data.Ord 
 import qualified Elements as Elements
 
-data Molekul = Molekul {content :: [Elements.Element]} 
+data Molekul = Molekul {content :: [(Elements.Element, Int)]} 
 
 instance Show Molekul where
     show (Molekul cnt) = 
         foldl (\str (atom, num) -> str ++ atom ++ if num > 1 then show num else "") "" lst
-        where sorted = map (\xs@(x:_) -> ((Elements.name x), length xs)) . group . sort $ cnt
+        where sorted = map (\(atom, num) -> ((Elements.name atom), num)) cnt
               lst    = sortBy (comparing snd) sorted
     
 
