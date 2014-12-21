b24c53381b632b8f0b87f116d53c745ab0b146ba
Paul Kasko <KaskoPaul.mail@gmail.com>
2014-12-21 20:50:38 +0100

    Added some easier to-understand definitions

--- src/Molekuls.hs
+++ src/Molekuls.hs
@@ -5,12 +5,13 @@ import Data.List
 import Data.Ord 
 import qualified Elements as Elements
 
-data Molekul = Molekul {content :: [(Elements.Element, Int)]} 
+type Number = Int
+data Molekul = Molekul {content :: [(Elements.Element, Number)]} 
 
 instance Show Molekul where
     show (Molekul cnt) = 
         foldl (\str (atom, num) -> str ++ atom ++ if num > 1 then show num else "") "" lst
-        where sorted = map (\(atom, num) -> ((Elements.name atom), num)) cnt
+        where sorted = map (\(atom, num) -> (Elements.name atom, num)) cnt
               lst    = sortBy (comparing snd) sorted
     
 
