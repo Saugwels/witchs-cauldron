7cc79aed2b9fb1dfbe5c38fa3cf7beb045cfb3ac
Paul Kasko <KaskoPaul.mail@gmail.com>
2014-12-24 16:57:32 +0100

    rewrote some unefficient parts

--- src/Salt.hs
+++ src/Salt.hs
@@ -5,20 +5,20 @@ import qualified Element as Element
 
 data Ion  = Ion {name :: String, charge :: Int} deriving (Eq)
 data Salt = Salt {kation :: Kation, factorKa :: Int
-                 ,anion :: Anion, factorAn :: Int} 
+                 ,anion :: Anion, factorAn :: Int}
 
 type Kation = Ion
 type Anion  = Ion
 
 instance Show Ion where
-    show (Ion ion charging) = ion ++ show (abs charging) 
+    show (Ion ion charging) = ion ++ show (abs charging)
                            ++ (if charging > 0 then "+" else "-")
 
 instance Show Salt where
-    show (Salt ka faKa an faAn) = name ka 
+    show (Salt ka faKa an faAn) = name ka
         ++ (if faKa > 1 && faKa /= faAn then show faKa else "")
-        ++ (if faAn > 1 && faKa /= faAn then 
-            if an `elem` moleculIons then 
+        ++ (if faAn > 1 && faKa /= faAn then
+            if an `elem` moleculIons then
                 ("(" ++ name an ++ ")" ++ show faAn)
                     else (name an ++ show faAn)
             else (name an ++ ""))
@@ -29,9 +29,13 @@ empiricalFormula metal@(Ion ka kaCharge) nonmetal@(Ion an anCharge)=
          ,anion = nonmetal, factorAn = kaCharge}
 
 elementToIon :: Element.Element -> Ion
-elementToIon (Element.Element ename p gp) = 
-    if p==8 then error "No reation" else Ion {name=ename, charge=ch}
+elementToIon (Element.Element ename p gp) =
+    if p==8 then error "Noble Gases won't be Ions!" else Ion {name=ename, charge=ch}
     where ch = if p <= 3 then p else p-8
+
+ionToElement :: Ion -> Element.Element
+ionToElement ion = Element.Element {Element.name=(name ion), Element.period=endch, Element.group=2}
+        where endch = if (charge ion) <= 3 then (charge ion) else (charge ion)+8
 
 -- Molecular ions
 carbonat = Ion {name = "CO3", charge = -2}
@@ -43,4 +47,3 @@ hydrogencarbonat = Ion {name = "HCO3", charge = -1}
 hydrogensulfat   = Ion {name = "HSO4", charge = -1}
 
 moleculIons = [carbonat, sulfat, nitrat, phosphat, hydrogenoxid, hydrogencarbonat, hydrogensulfat]
-
